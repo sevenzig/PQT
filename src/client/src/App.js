276efa8f8cf748268cbfbc928b551a1c4f7783e9
sevenzig <j.a.scott.thurston@gmail.com>
2024-10-18 15:01:05 -0400

    initial commit

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,25 +1,5 @@
 // src/client/src/App.js
-// version 1.02
-
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import FileUpload from './components/FileUpload';
-import DataReview from './components/DataReview';
-import QRScanner from './components/QRScanner';
-
-function App() {
-  const [manufacturer, setManufacturer] = useState('');
-  const [product, setProduct] = useState('');
-
-  const handleSearch = () => {
-    // Implement your search logic here
-    // You can use the `manufacturer` and `product` state variables to filter the data
-    console.log(`Searching for ${manufacturer} - ${product}`);
-  };
-
-  return (
-    <Router>// src/client/src/App.js
-// version 1.03
+// version 1.04
 
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
@@ -80,54 +60,6 @@ function App() {
             value={lotNumber}
             onChange={(e) => setLotNumber(e.target.value)}
           />
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/file-upload">File Upload</Link>
-            </li>
-            <li>
-              <Link to="/data-review">Data Review</Link>
-            </li>
-            <li>
-              <Link to="/qr-scanner">QR Scanner</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/file-upload">
-            <FileUpload />
-          </Route>
-          <Route path="/data-review">
-            <DataReview />
-          </Route>
-          <Route path="/qr-scanner">
-            <QRScanner />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-
-      <div>
-        <h1>Probiotic Quality Tracker</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Manufacturer"
-            value={manufacturer}
-            onChange={(e) => setManufacturer(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Product"
-            value={product}
-            onChange={(e) => setProduct(e.target.value)}
-          />
-          <button onClick={handleSearch}>Search</button>
         </div>
         <nav>
           <ul>
